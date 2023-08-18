0cb86cb17374283d4bed6a595a2ad751cb1ca90e
Rodolfo Delduca <84294800+UsoDelduca@users.noreply.github.com>
2023-08-19 10:15:39 +1200

    style file working

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ const server = express()
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
+const publicFolder = Path.resolve('public')
+server.use(express.static(publicFolder))
+
 // Middleware
 server.engine('hbs', handlebars.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
